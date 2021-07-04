7c566154cd146ab3db2b3d9683022e0193e7e893
owiesalomari <owies.omari3@gmail.com>
2021-07-04 04:55:24 +0300

    Reading halls from url using volley

--- app/src/main/java/com/example/smartwedding/HallsListActivity.kt
+++ app/src/main/java/com/example/smartwedding/HallsListActivity.kt
@@ -7,28 +7,53 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.smartwedding.databinding.ActivityHallsListBinding
 import com.example.smartwedding.models.Hall
 import com.example.smartwedding.util.*
-import org.json.JSONArray
+import org.json.JSONException
+import com.example.smartwedding.util.VolleyCallBack
+import org.json.JSONObject
 
 class HallsListActivity : AppCompatActivity() {
 
     private val halls = ArrayList<Hall>()
     private lateinit var binding: ActivityHallsListBinding
-    private val HALLS_FILE_NAME="halls.json"
+    private val hallsDataURL = "https://api.npoint.io/6045ed449416133135c1"
+    private var requestQueue: RequestQueue? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_halls_list)
-        getHallsData()
-        bindHallsList()
-        binding.halls.setOnItemClickListener(){ adapterView, _, position, _ ->
+        requestQueue = Volley.newRequestQueue(this)
+        getHallsData(object : VolleyCallBack {
+            override fun onSuccess(response: JSONObject) {
+                val hallsArray = response.getJSONArray("halls")
+                for (i in 0 until hallsArray.length()) {
+                    var hall = Hall()
+                    val hallObject = hallsArray.getJSONObject(i)
+                    hall.apply {
+                        name = hallObject.getString(HALL_NAME)
+                        phone = hallObject.getString(HALL_PHONE)
+                        location = hallObject.getString(HALL_CITY)
+                        capacity = hallObject.getInt(HALL_CAPACITY)
+                        price = hallObject.getInt(HALL_PRICE)
+                        childrenAllowed = hallObject.getBoolean(CHILDREN_ALLOWED)
+                        imageId = hallObject.getString(HALL_IMAGE_ID)
+                        halls.add(hall)
+                    }
+                }
+                binding.progressBar.visibility = ProgressBar.INVISIBLE
+                bindHallsList()
+            }
+        })
+        binding.halls.setOnItemClickListener() { adapterView, _, position, _ ->
             hallItemsOnClickListener(adapterView.getItemAtPosition(position))
         }
     }
@@ -40,63 +65,58 @@ class HallsListActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        startActivity(Intent(this,CartActivity::class.java))
+        startActivity(Intent(this, CartActivity::class.java))
         return super.onOptionsItemSelected(item)
     }
 
-   private fun bindHallsList() {
-       intent.extras?.let {
-           val hallPrice= it.getInt(HALL_PRICE)
-           val hallCapacity=it.getInt(HALL_CAPACITY)
-           val childrenAllowed=it.getBoolean(CHILDREN_ALLOWED)
-           val hallCity=it.get(HALL_CITY).toString()
-           var imageIds = ArrayList<Int>()
-           var hallNames=ArrayList<String>()
-           halls.forEach {hall->
-               if (hallPrice <=hall.price  && hall.capacity>= hallCapacity &&hall.location==hallCity&&
-                   hall.childrenAllowed==childrenAllowed) {
-                   hallNames.add(hall.name)
-                   imageIds.add(resources.getIdentifier(hall.imageId, "drawable", packageName))
-               }
-           }
-           binding.halls.adapter = MyListAdapter(this,hallNames,imageIds)
-       }
-    }
-
-   private fun getHallsData(){
-        var hallsArray = JSONArray(applicationContext.assets.open(HALLS_FILE_NAME).bufferedReader().use { it.readText() })
-        for (jsonIndex in 0 until hallsArray.length()) {
-            var hall=Hall()
-            hall.apply {
-                name = hallsArray.getJSONObject(jsonIndex).getString(HALL_NAME)
-                phone = hallsArray.getJSONObject(jsonIndex).getString(HALL_PHONE)
-                location = hallsArray.getJSONObject(jsonIndex).getString(HALL_CITY)
-                capacity = hallsArray.getJSONObject(jsonIndex).getInt(HALL_CAPACITY)
-                price = hallsArray.getJSONObject(jsonIndex).getInt(HALL_PRICE)
-                childrenAllowed = hallsArray.getJSONObject(jsonIndex).getBoolean(CHILDREN_ALLOWED)
-                imageId=hallsArray.getJSONObject(jsonIndex).getString(HALL_IMAGE_ID)
-                halls.add(hall)
+    private fun bindHallsList() {
+        intent.extras?.let {
+            val hallPrice = it.getInt(HALL_PRICE)
+            val hallCapacity = it.getInt(HALL_CAPACITY)
+            val childrenAllowed = it.getBoolean(CHILDREN_ALLOWED)
+            val hallCity = it.get(HALL_CITY).toString()
+            val imageIds = ArrayList<Int>()
+            val hallNames = ArrayList<String>()
+            halls.forEach { hall ->
+                if (hallPrice <= hall.price && hall.capacity >= hallCapacity && hall.location == hallCity &&
+                    hall.childrenAllowed == childrenAllowed) {
+                    hallNames.add(hall.name)
+                    imageIds.add(resources.getIdentifier(hall.imageId, "drawable", packageName))
+                }
             }
+            binding.halls.adapter = MyListAdapter(this, hallNames, imageIds)
         }
     }
 
-   private fun hallItemsOnClickListener( itemAtPosition:Any){
-       val hall= (halls.filter { it.name==itemAtPosition })[0]
-       val intent=Intent(this,HallDescriptionActivity::class.java)
-       intent.putExtra(HALL_NAME,hall.name)
-       intent.putExtra(HALL_CITY,hall.location)
-       intent.putExtra(HALL_CAPACITY,hall.capacity)
-       intent.putExtra(HALL_PRICE,hall.price)
-       intent.putExtra(CHILDREN_ALLOWED,hall.childrenAllowed)
-       intent.putExtra(HALL_IMAGE_ID,hall.imageId)
-       intent.putExtra(HALL_PHONE,hall.phone)
-       startActivity(intent)
+    private fun getHallsData(callBack: VolleyCallBack) {
+        val request = JsonObjectRequest(Request.Method.GET, hallsDataURL, null, { response ->
+            try {
+                callBack.onSuccess(response)
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+        }, { error -> error.printStackTrace() })
+        requestQueue?.add(request)
+    }
+
+    private fun hallItemsOnClickListener(itemAtPosition: Any) {
+        val hall = (halls.filter { it.name == itemAtPosition })[0]
+        val intent = Intent(this, HallDescriptionActivity::class.java)
+        intent.putExtra(HALL_NAME, hall.name)
+        intent.putExtra(HALL_CITY, hall.location)
+        intent.putExtra(HALL_CAPACITY, hall.capacity)
+        intent.putExtra(HALL_PRICE, hall.price)
+        intent.putExtra(CHILDREN_ALLOWED, hall.childrenAllowed)
+        intent.putExtra(HALL_IMAGE_ID, hall.imageId)
+        intent.putExtra(HALL_PHONE, hall.phone)
+        startActivity(intent)
     }
 }
 
-class MyListAdapter(private val context: Activity, private val title: ArrayList<String>,
-                    private val imgid: ArrayList<Int>)
-    : ArrayAdapter<String>(context, R.layout.activity_listview, title) {
+class MyListAdapter(
+    private val context: Activity, private val title: ArrayList<String>,
+    private val imgid: ArrayList<Int>
+) : ArrayAdapter<String>(context, R.layout.activity_listview, title) {
     override fun getView(position: Int, view: View?, parent: ViewGroup): View {
         val inflater = context.layoutInflater
         val rowView = inflater.inflate(R.layout.activity_listview, null, true)
---
+++ app/src/main/java/com/example/smartwedding/util/VolleyCallBack.kt
@@ -0,0 +1,11 @@
+package com.example.smartwedding.util
+
+import org.json.JSONObject
+
+
+
+
+interface VolleyCallBack {
+    fun onSuccess(result:JSONObject )
+
+}
